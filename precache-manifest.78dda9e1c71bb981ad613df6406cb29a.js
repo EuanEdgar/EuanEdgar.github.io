4b15bbebd68d42fe24d5811f336f9e7f520cf542
EuanEdgar <euan@spicerack.co.uk>
2021-07-19 16:17:46 +0100

    Build workflow push works

---
+++ precache-manifest.78dda9e1c71bb981ad613df6406cb29a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: '2d2173b6f12b843ba75a',
+    url: '/css/app.0823e53f.css',
+  },
+  {
+    revision: 'badb117a5f66cd1a023c60f7b26055df',
+    url: '/index.html',
+  },
+  {
+    revision: '2d2173b6f12b843ba75a',
+    url: '/js/app.fd3cf07c.js',
+  },
+  {
+    revision: 'c0141cd72fb4812d68ee',
+    url: '/js/chunk-vendors.c06f9603.js',
+  },
+  {
+    revision: '18437c1ccbb6ea0c090a4f85fe7c88ab',
+    url: '/manifest.json',
+  },
+  {
+    revision: 'b6216d61c03e6ce0c9aea6ca7808f7ca',
+    url: '/robots.txt',
+  },
+]);
