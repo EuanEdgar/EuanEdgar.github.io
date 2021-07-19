0f211fe0d88ca1fc2474dff745ccc1f1cca4030e
EuanBot <euan@spicerack.co.uk>
2021-07-19 15:56:57 +0000

    Build files

---
+++ docs/precache-manifest.9f202dbe2311e96576a35fedf6332f55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b4a513760c0568275e",
+    "url": "/css/app.601d2ada.css"
+  },
+  {
+    "revision": "708403fd8deb83793f5fd8eb134a8ecc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22b4a513760c0568275e",
+    "url": "/js/app.a4f27120.js"
+  },
+  {
+    "revision": "c0141cd72fb4812d68ee",
+    "url": "/js/chunk-vendors.c06f9603.js"
+  },
+  {
+    "revision": "4bdf8d6ada4ed526229a68694432eb96",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
